a024fa599f0dc71ce7cd12ada1ffe8e66ebaaf67
chachaprimeofficial-max <chachaprimeofficial@gmail.com>
2026-09-23 06:13:05 +0500

    Add payment model

---
+++ app/Models/Payment.php
@@ -0,0 +1,9 @@
+<?php
+namespace AppModels;
+use IlluminateDatabaseEloquentModel;
+class Payment extends Model {
+ protected $fillable=['order_id','user_id','method_id','transaction_reference','amount','currency','status','gateway_response','paid_at'];
+ protected $casts=['gateway_response'=>'array','amount'=>'decimal:2','paid_at'=>'datetime'];
+ public function order(){return $this->belongsTo(Order::class);}
+ public function user(){return $this->belongsTo(User::class);}
+}
