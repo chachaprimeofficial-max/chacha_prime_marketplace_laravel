a668e71e8cba022f79215c8f16530c35fedd85c3
chachaprimeofficial-max <chachaprimeofficial@gmail.com>
2026-09-23 06:10:46 +0500

    Add Laravel public entrypoint

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php
+
+use IlluminateFoundationApplication;
+use IlluminateHttpRequest;
+
+define('LARAVEL_START', microtime(true));
+
+require __DIR__.'/../vendor/autoload.php';
+
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$app->handleRequest(Request::capture());
