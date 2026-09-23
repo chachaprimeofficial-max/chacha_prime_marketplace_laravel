a62445ecf0f3455dcbc3c8eaaa0a4c3952c2eb2f
chachaprimeofficial-max <chachaprimeofficial@gmail.com>
2026-09-23 11:05:00 +0500

    Use persistent Seller Center sidebar layout

--- resources/views/vendor/wallet.blade.php
+++ resources/views/vendor/wallet.blade.php
@@ -1,3 +1,3 @@
-@extends('layouts.storefront')
+@extends('vendor.layout')
 @section('title','Vendor Wallet')
 @section('content')<div class="container"><h1>Wallet</h1><div class="panel"><div class="stat"><small>Available Balance</small><strong>{{number_format($wallet->balance ?? 0,2)}}</strong></div><p>Payouts and wallet transactions are controlled by the marketplace.</p></div></div>@endsection
