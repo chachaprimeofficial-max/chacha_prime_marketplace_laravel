ee2ad959b3aae466d7b0621cb226a72a0a18b00e
chachaprimeofficial-max <chachaprimeofficial@gmail.com>
2026-09-24 03:28:53 +0500

    Fix header category menu and replace emoji UI icons

--- resources/views/partials/mega-menu.blade.php
+++ resources/views/partials/mega-menu.blade.php
@@ -1,24 +1,15 @@
 <div class="cp-drawer-overlay" data-cp-overlay></div>
 <aside class="cp-drawer" data-cp-drawer aria-hidden="true">
-<div class="cp-drawer-head"><strong>☰ Shop by Category</strong><button type="button" data-cp-close-drawer aria-label="Close menu">×</button></div>
-<div class="cp-drawer-body">
-<div class="cp-drawer-home"><a href="{{route('shop')}}">All Products</a></div>
+<div class="cp-drawer-head"><strong><span class="cp-hamburger-icon"></span> Shop by Category</strong><button type="button" data-cp-close-drawer aria-label="Close menu">×</button></div>
+<div class="cp-drawer-body"><div class="cp-drawer-home"><a href="{{route('shop')}}">All Products</a></div>
 @foreach(($headerCategories??collect()) as $category)
 <div class="cp-category-group" data-cp-category-group>
-<button type="button" class="cp-category-title" data-cp-category-trigger aria-expanded="false">
-<span>{{$category->name}}</span><span class="cp-category-arrow">›</span>
-</button>
 @if($category->children && $category->children->count())
-<div class="cp-subcategories" data-cp-subcategories>
-<a class="cp-view-all" href="{{route('shop')}}?category={{$category->id}}">Shop all {{$category->name}}</a>
-@foreach($category->children as $child)
-<a href="{{route('shop')}}?category={{$child->id}}">{{$child->name}}</a>
-@endforeach
-</div>
+<button type="button" class="cp-category-title" data-cp-category-trigger aria-expanded="false"><span>{{$category->name}}</span><span class="cp-category-arrow">›</span></button>
+<div class="cp-subcategories" data-cp-subcategories><a class="cp-view-all" href="{{route('shop')}}?category={{$category->id}}">Shop all {{$category->name}}</a>@foreach($category->children as $child)<a href="{{route('shop')}}?category={{$child->id}}">{{$child->name}}</a>@endforeach</div>
 @else
-<a class="cp-category-direct" href="{{route('shop')}}?category={{$category->id}}"></a>
+<a class="cp-category-direct cp-category-direct-link" href="{{route('shop')}}?category={{$category->id}}"><span>{{$category->name}}</span><span>›</span></a>
 @endif
 </div>
 @endforeach
-</div>
-</aside>
+</div></aside>
