3c9e694ba0dd2f00af46368d38dd16e8451afc88
chachaprimeofficial-max <chachaprimeofficial@gmail.com>
2026-09-23 06:12:52 +0500

    Add vendor model

---
+++ app/Models/Vendor.php
@@ -0,0 +1,9 @@
+<?php
+namespace AppModels;
+use IlluminateDatabaseEloquentModel;
+class Vendor extends Model {
+ protected $fillable=['user_id','business_name','legal_name','country','registration_number','tax_number','status','verification_status','logo','description','commission_rate'];
+ protected $casts=['commission_rate'=>'decimal:3'];
+ public function user(){return $this->belongsTo(User::class);}
+ public function products(){return $this->hasMany(Product::class);}
+}
