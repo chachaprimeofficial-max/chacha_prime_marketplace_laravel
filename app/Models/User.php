39d43ccdd1bfbb22bc07c3025c5e699faa85f50a
chachaprimeofficial-max <chachaprimeofficial@gmail.com>
2026-09-24 13:33:17 +0500

    Add country relationships to marketplace models

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,10 +4,11 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 class User extends Authenticatable {
  use Notifiable;
- protected $fillable=['name','email','password','role','status','phone','avatar','locale','totp_secret','two_factor_enabled'];
+ protected $fillable=['name','email','password','role','status','phone','avatar','locale','country_id','totp_secret','two_factor_enabled'];
  protected $hidden=['password','remember_token','totp_secret'];
  protected function casts(): array {return ['email_verified_at'=>'datetime','password'=>'hashed','two_factor_enabled'=>'boolean'];}
  public function otpCodes(){return $this->hasMany(OtpCode::class);}
  public function vendor(){return $this->hasOne(Vendor::class);}
+ public function country(){return $this->belongsTo(Country::class);}
  public function wallet(){return $this->hasOne(Wallet::class);}
 }
